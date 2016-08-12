908e93c8e04a459282327ff6cba9a779148cab84
Nick Balestra <nbalestra@opentable.com>
2016-08-12 14:34:10 +0100

    added a chokidar watcher and socket.io server

--- app/index.js
+++ app/index.js
@@ -1,9 +1,15 @@
-const port = process.env.PORT || 8000
+const chokidarSocketEmitter = require('chokidar-socket-emitter')
 const express = require('express')
-const server = express()
+const http = require('http')
 
+const server = express()
+const port = process.env.PORT || 8000
+
+server.set('port', port)
 server.use(express.static(`${__dirname}/client`))
 server.get('/', (res, req) => res.sendFile(`${__dirname}/client/index.html`))
+
+if (server.get('env') === 'development') chokidarSocketEmitter({app: http.createServer(server)})
 
 server.listen(port, () => console.log(
   `👟  Server running in ${server.get('env')} mode\n` +
